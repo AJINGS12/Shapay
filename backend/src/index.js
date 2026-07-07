3db1443b1403f327d3e05c114712a97a26559d74
AJINGS12 <ismailajingi737@gmail.com>
2026-07-07 22:30:15 +0700

    fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
-
-process.on('uncaughtException', (err) => {
-  console.error('UNCAUGHT EXCEPTION:', err.message);
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:");
+  console.error(err.message);
   console.error(err.stack);
 });
 
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("UNHANDLED REJECTION:");
+  console.error(reason);
+});
 
 require("dotenv").config();
 
@@ -11,71 +15,150 @@ console.log("NOMBA ENV CHECK:", {
   NOMBA_BASE_URL: process.env.NOMBA_BASE_URL,
   CLIENT_ID_EXISTS: !!process.env.NOMBA_CLIENT_ID,
   CLIENT_SECRET_EXISTS: !!process.env.NOMBA_CLIENT_SECRET,
-  ACCOUNT_ID_EXISTS: !!process.env.NOMBA_ACCOUNT_ID,
+  PARENT_ACCOUNT_EXISTS: !!process.env.NOMBA_PARENT_ACCOUNT_ID,
 });
 
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-
-const emailPreviewRoute = require("./routes/emailPreviewRoute");
-const aiRoutes = require("./routes/aiRoutes");
-const simulationRoutes = require("./routes/simulationRoutes");
-const recoveryAnalyticsRoutes = require("./routes/recoveryAnalyticsRoutes");
 
 const app = express();
 
-app.use(cors());
 
+// REQUEST LOGGER
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+
+// CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
+
+
+// BODY PARSER
 app.use(express.json());
 
+
+// ROOT
 app.get("/", (req, res) => {
   res.send("Shapay API running");
 });
 
+
+// WEBHOOKS
 const nombaWebhook = require("./webhooks/nombaWebhook");
+
 app.use("/webhooks", nombaWebhook);
 app.use("/webhook", nombaWebhook);
 
+
+// AUTH TEST
 const { getAccessToken } = require("./services/nombaAuthService");
 
 app.get("/test-token", async (req, res) => {
   try {
     const token = await getAccessToken();
-    res.json({ success: true, message: "Nomba authentication successful" });
+
+    res.json({
+      success: true,
+      message: "Nomba authentication successful",
+      tokenExists: !!token,
+    });
+
   } catch (error) {
-    res.status(500).json({ success: false, message: "Failed to generate token" });
+
+    console.error("TEST TOKEN ERROR:");
+
+    if (error.response) {
+      console.error(
+        JSON.stringify(error.response.data, null, 2)
+      );
+    } else {
+      console.error(error.message);
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to generate token",
+    });
   }
 });
 
-const { processRecurringBilling } = require("./services/subscriptionBillingService");
+
+// ROUTES
 const paymentRoutes = require("./routes/paymentRoutes");
 const subscriptionRoutes = require("./routes/subscriptionRoutes");
-const analyticsRoutes = require("./routes/analyticsRoutes");
-const reportingRoutes = require("./routes/reportingRoutes");
 
 app.use("/payments", paymentRoutes);
 app.use("/subscriptions", subscriptionRoutes);
-app.use("/analytics", analyticsRoutes);
-app.use("/reports", reportingRoutes);
-app.use(emailPreviewRoute);
-app.use(aiRoutes);
-app.use(simulationRoutes);
-app.use(recoveryAnalyticsRoutes);
 
-setInterval(() => {
-  processRecurringBilling();
-}, 60 * 1000);
 
+// TEMPORARILY DISABLED ROUTES
+// const analyticsRoutes = require("./routes/analyticsRoutes");
+// const reportingRoutes = require("./routes/reportingRoutes");
+// const emailPreviewRoute = require("./routes/emailPreviewRoute");
+// const aiRoutes = require("./routes/aiRoutes");
+// const simulationRoutes = require("./routes/simulationRoutes");
+// const recoveryAnalyticsRoutes = require("./routes/recoveryAnalyticsRoutes");
+
+// app.use("/analytics", analyticsRoutes);
+// app.use("/reports", reportingRoutes);
+// app.use(emailPreviewRoute);
+// app.use(aiRoutes);
+// app.use(simulationRoutes);
+// app.use(recoveryAnalyticsRoutes);
+
+
+// TEMPORARILY DISABLED BILLING JOB
+// const {
+//   processRecurringBilling,
+// } = require("./services/subscriptionBillingService");
+
+// setInterval(() => {
+//   processRecurringBilling();
+// }, 60 * 1000);
+
+
+// CALLBACK ROUTES
 app.get("/payment/callback", (req, res) => {
-  res.json({ success: true, message: "Payment completed" });
+  res.json({
+    success: true,
+    message: "Payment completed",
+  });
 });
 
 app.get("/payments/callback", (req, res) => {
-  res.json({ success: true, message: "Payment completed" });
+  res.json({
+    success: true,
+    message: "Payment completed",
+  });
 });
 
+
+// GLOBAL ERROR HANDLER
+app.use((err, req, res, next) => {
+
+  console.error("GLOBAL ERROR:");
+  console.error(err);
+
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal server error",
+  });
+});
+
+
+// SERVER
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
