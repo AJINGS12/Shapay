4aa5a59feb47a1104493960cac1d63b6a596a33a
AJINGS12 <ismailajingi737@gmail.com>
2026-07-09 13:31:50 +0700

    Allow x-merchant-id header in CORS config

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,7 +36,7 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-merchant-id"],
   })
 );
 
